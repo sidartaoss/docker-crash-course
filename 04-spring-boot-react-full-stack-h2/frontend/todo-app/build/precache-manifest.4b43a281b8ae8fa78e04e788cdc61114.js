986affbbdca8f7812525aad03466936f5a425167
sidartaoss <semprebono@gmail.com>
2020-06-26 16:38:34 -0300

    Add Docker Crash Course

---
+++ 04-spring-boot-react-full-stack-h2/frontend/todo-app/build/precache-manifest.4b43a281b8ae8fa78e04e788cdc61114.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "7b66d5e3e143f1cc9c18",
+    "url": "/static/js/main.6b07574d.chunk.js"
+  },
+  {
+    "revision": "3be61595d9a1137a984f",
+    "url": "/static/js/2.3f64e426.chunk.js"
+  },
+  {
+    "revision": "7b66d5e3e143f1cc9c18",
+    "url": "/static/css/main.748c2d77.chunk.css"
+  },
+  {
+    "revision": "55ae39528a6a938a7071564aba4d3909",
+    "url": "/index.html"
+  }
+];
